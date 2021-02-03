d543c18855d7ff6b8056df83b494d8c274c54d27
pgomezboza <pgomezboza@gmail.com>
2021-02-03 22:39:28 +0000

    add file: 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  print a message to display
+ * Return: 0 on success, otherwise not 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
