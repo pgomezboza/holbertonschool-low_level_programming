fc20f0d81a5704906c8c090f7a54edb1d8e2fe41
pgomezboza <pgomezboza@gmail.com>
2021-02-03 22:59:55 +0000

    add file: 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print a messange to display
+ * Return: 0 if success, other case distinct 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
