908edaa13ac6e1bec981395d0f949065cb82121b
pgomezboza <pgomezboza@gmail.com>
2021-02-05 19:51:25 +0000

    add file: 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a main function
+ * Description: print the lowercase alphabet in reverse order.
+ * Return: default 0
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(c);
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
