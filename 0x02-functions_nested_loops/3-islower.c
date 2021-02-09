d6bb70196a76163ab02038476c3682fb8052ea2d
pgomezboza <pgomezboza@gmail.com>
2021-02-09 20:21:15 +0000

    update file 3-islower.c, corrects of Betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,15 @@
 /**
  * _islower - request to input a value
  * @c: int value.
- * Description: 
+ * Description:
  * Return: '1' if lowercase, '0' if not lowercase
  */
- 
+
 int _islower(int c)
 {
 	int r = 0;
 
-	(c >= 97 && c <= 122) ? r = 1 : r ;
+	(c >= 97 && c <= 122) ? r = 1 : r;
 
-	return(r);
+	return (r);
 }
