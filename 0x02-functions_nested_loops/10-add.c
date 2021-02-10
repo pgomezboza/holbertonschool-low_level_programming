b24e3ec6efe3708690d69c7703766ad6930d228d
pgomezboza <pgomezboza@gmail.com>
2021-02-10 00:40:41 +0000

    add file: 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - add two integers
+ * @a: two integer arguments
+ * @b: two integer arguments
+ * Return: r as sum
+ */
+
+int add(int a, int b)
+{
+	int r = a + b;
+
+	return (r);
+}
