8fb2ccdf9a68232b97eeff43f8e56b50a5b27748
pgomezboza <pgomezboza@gmail.com>
2021-02-11 15:18:34 -0500

    add file: 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - function that multiplies two integers.
+ * @n: int type number.
+ * @m: int type number.
+ * Description:
+ * Return: result 'r' of multiplication.
+ */
+
+int mul(int n, int m)
+{
+	int r = n * m;
+
+	return (r);
+}
