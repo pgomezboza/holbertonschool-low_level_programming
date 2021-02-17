605b2138b5b7bec9e3033edcbad1e0c236faa36b
pgomezboza <pgomezboza@gmail.com>
2021-02-16 19:02:28 -0500

    update file: 5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rev_string - function that reverses a string.
+ * @s: char array string type.
+ */
+
+void rev_string(char *s)
+{
+	int n = 0, ln1 = 0, ln2 = 0;
+	char t = ' ';
+
+	while (s[ln1] != '\0')
+	{
+		ln1++;
+	}
+
+	ln2 = ln1 - 1;
+
+	for (n = 0; n < ln1 / 2; ln2--,  n++)
+	{
+		t = s[n];
+		s[n] = s[ln2];
+		s[ln2] = t;
+	}
+}
+
+
