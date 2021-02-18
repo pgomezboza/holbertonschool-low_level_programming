d18f57cb12083cafcc9d02567943ddc837b0db4b
pgomezboza <pgomezboza@gmail.com>
2021-02-18 18:34:16 -0500

    upodate file: 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,12 +13,12 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i=0; dest[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 		;
 	}
 
-	for (j=0; src[j] != '\0' && j < n; i++, j++)
+	for (j = 0; src[j] != '\0' && j < n; i++, j++)
 	{
 		dest[i] = src[j];
 	}
