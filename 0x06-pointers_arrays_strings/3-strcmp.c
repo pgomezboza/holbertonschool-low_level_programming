67934acc81363d8d44930ebfdc223757e577d924
pgomezboza <pgomezboza@gmail.com>
2021-02-18 20:09:35 -0500

    update file: 3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: char type string
+ * @s2: char type string
+ *
+ * Return: negative, positive or zero.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int n, r = 0;
+
+	for (n = 0; s1[n] != '\0' || s2[n] != '\0'; n++)
+	{
+		if (s1[n] != s2[n])
+		{
+			r = (s1[n] - s2[n]);
+		}
+	}
+
+	return (r);
+}
