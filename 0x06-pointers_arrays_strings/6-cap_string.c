cb464b45bd01a63759f040db555ec36292c8ee88
pgomezboza <pgomezboza@gmail.com>
2021-02-19 13:00:34 -0500

    add file: 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * cap_string - function capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+
+char *cap_string(char *s)
+{
+	int n, m;
+
+	char spec_ch[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		if (n == 0 && s[n] >= 'a' && s[n] <= 'z')
+			s[n] -= 32;
+
+		for (m = 0; m < 13; m++)
+		{
+			if (s[n] == spec_ch[m])
+			{
+				if (s[n + 1] >= 'a' && s[n + 1] <= 'z')
+				{
+					s[n + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
