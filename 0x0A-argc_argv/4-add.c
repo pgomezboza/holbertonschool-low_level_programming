677b73cc8189b3f493470c71412697c56d59623e
pgomezboza <pgomezboza@gmail.com>
2021-02-28 18:59:40 -0500

    update file: 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that adds positive numbers.
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Description: If no number is passed to program, print 0.
+ * If one of the numbers contain non-digits, print Error.
+ * Return: 1 if error, 0 if function runs properly.
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, count, num = 0;
+	char *ch;
+
+	if (argc > 1)
+	{
+		for (count = 1; argv[cout]; x++)
+		{
+			num = strtol(argv[count], &count, 10);
+			if (!*ch)
+			{
+				sum += num;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
