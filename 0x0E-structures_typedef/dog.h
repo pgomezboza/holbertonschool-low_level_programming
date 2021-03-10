31ff3a30259aa3ec5bcaca204078ca8046344a34
pgomezboza <pgomezboza@gmail.com>
2021-03-09 20:24:11 -0500

    upload file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
