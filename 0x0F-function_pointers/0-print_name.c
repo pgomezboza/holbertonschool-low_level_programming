bf83b312cb02459227c136eb03c0bf5ce0eb5a99
pgomezboza <pgomezboza@gmail.com>
2021-03-11 18:12:11 -0500

    update files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - prints a name from a function pointer.
  * @name: char string, name to print.
-	* @f: function pointer that takes a string as an argument.
+ * @f: function pointer that takes a string as an argument.
  */
 
 void print_name(char *name, void (*f)(char *))
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
-#ifndef _FUNCTION_POINTERS_H_
-
-#define _FUNCTION_POINTERS_H_
+#ifndef	FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 #include <stdlib.h>
 #include <stddef.h>
 
