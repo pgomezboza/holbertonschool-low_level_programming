a96903e7e2c88a3006c82d4b627775c321e86b3b
pgomezboza <pgomezboza@gmail.com>
2021-03-11 23:54:37 -0500

    upload files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,46 @@
+#include "3-calc.h"
+
+/**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, r;
+	char op;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	op = *argv[2];
+
+	if ((op == '/' || op == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	r = func(a, b);
+
+	printf("%d\n", r);
+
+	return (0);
+}
