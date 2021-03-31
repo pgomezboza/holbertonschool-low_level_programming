e3e69367db9c68da8127f364bbbda43cb4cba711
pgomezboza <pgomezboza@gmail.com>
2021-03-30 22:05:20 -0500

    update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,9 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
+	if (fd == -1)
+		return (-1);
+
 	if (text_content != NULL)
 	{
 		while (text_content[n] != '\0')
