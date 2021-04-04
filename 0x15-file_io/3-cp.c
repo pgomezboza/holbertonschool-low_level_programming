e0f682d3495ecf1a5cc03d455ab7f63ce53de70c
pgomezboza <pgomezboza@gmail.com>
2021-04-03 23:04:05 -0500

    update file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,67 @@
+#include "holberton.h"
+
+/**
+ * main - copies the content of a file to another file.
+ * @argc: number of arguments passed.
+ * @argv: array of pointers to the arguments.
+ *
+ * Return: 0 for success and exit codes for failure.
+ */
+int main(int argc, char *argv[])
+{
+	int to = 0, from = 0, to_count = 0;
+	int from_count = 1024;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO,
+			"Usage: cp file_from file_to\n");
+
+		exit(97);
+	}
+
+	from = open(argv[1], O_RDONLY);
+	if (from == 1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+
+		exit(98);
+	}
+
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (to == 1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", argv[2]);
+
+		exit(99);
+	}
+
+	while (from_count == 1024)
+	{
+		from_count = read(from, buffer, 1024);
+
+		if (from_count == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+
+			exit(98);
+		}
+
+		to_count = write(to, buffer, from_count);
+
+		if (to_count == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+
+			exit(99);
+		}
+	}
+
+	return (0);
+}
